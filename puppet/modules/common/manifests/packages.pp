d64ab6462cbf146be4e75a4c08aa1b73122d68ba
8bitrazzle <brianew20@yahoo.com>
2018-09-22 15:11:52 -0400

    adding common config

---
+++ puppet/modules/common/manifests/packages.pp
@@ -0,0 +1,18 @@
+class common::packages {
+
+    package {
+	'vim-enhanced':
+		ensure => present;
+	'net-tools':
+		ensure => present;
+	'git':
+		ensure => present;
+	'wget':
+		ensure => present;
+	'epel-release':
+		ensure => present;
+	'htop':
+		ensure => present,
+		require => Package['epel-release'];
+	}
+}
